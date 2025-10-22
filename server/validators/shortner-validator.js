84adc08b12edc52f951332602d3f8d12613bfe28
adityapandey78 <adityapandey.2402@gmail.com>
2025-10-22 18:39:24 +0530

    fixed the validator error

--- server/validators/shortner-validator.js
+++ server/validators/shortner-validator.js
@@ -8,8 +8,10 @@ export const shortnerSchema= z.object({
         .max(1024,{message:"URL cannot be this much longer"}),
         
     shortcode:z
-        .string({required_error:"Shortcode is required."})
+        .string()
         .trim()
         .min(3,{message:"Shortcode must be atleast 3 char long."})
-        .max(50,{message:"Shortcode too much long."}),               
+        .max(50,{message:"Shortcode too much long."})
+        .optional()
+        .or(z.literal('')), // Allow empty string            
 })
